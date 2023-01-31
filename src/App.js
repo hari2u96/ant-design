c0b6757c62376931f4e4504df120c963c1d4725e
hari2u96 <hari2u96@gmail.com>
2023-01-31 19:17:25 +0530

    Ant design

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import { Button ,Select,Radio,Input,Checkbox,Table,ConfigProvider, Space } from 'antd';
+import 'antd/dist/reset.css';
+import { useState } from 'react';
 
 function App() {
+  const  [currentTheme,setcurrentTheme]= useState ('light')
+  const lightTheme = {
+    colorPrimary:"green",
+    colorTextLightSolid:"White",
+    colorTextBase:"green"
+  }
+  const darkTheme ={
+    colorPrimary:"black",
+    colorTextLightSolid:"White",
+    colorTextBase:"black"
+  }
+  
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ConfigProvider
+      theme={{
+        token: currentTheme==='light'?lightTheme:darkTheme,
+      }}>
+        <Space direction='vertical' size={12}>
+          <Radio.Group value={currentTheme} onChange={(e)=>
+          {setcurrentTheme (e.target.value )}}  >
+            <Radio value={'light'}>Green</Radio>
+            <Radio value={'dark'}>Black</Radio>
+            </Radio.Group>
+        <Button type='primary'>Click me</Button>
+        <Radio>Radio</Radio>
+        <ConfigProvider
+        theme={{
+          inherit:false,
+          token:{
+            colorPrimary:"Purple"
+          }
+        }}>
+        <Radio>Radio with other color</Radio>
+        </ConfigProvider>
+        <Checkbox>Checkbox</Checkbox>
+        <Input placeholder='Type Here'/>
+        <Select placeholder='Select'/>
+        <Table columns={[{title:'column',dataIndex:'col'}]} dataSource={[{col:'value1'}]}/>
+        </Space>
+      </ConfigProvider>
     </div>
   );
 }
